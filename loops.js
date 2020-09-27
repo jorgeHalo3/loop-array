3d4a7da14263804310516c8d931c6ec2bddb4179
jorgeHalo3 <71196148+jorgeHalo3@users.noreply.github.com>
2020-09-27 13:03:12 -0400

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,23 @@
+for(let i =1000; i>= 0; i--){
+    if (i % 2 === 0){
+        console.log("even!");
+    }else {
+        console.log(i)}
+}
+
+for(let i = 0; i<=1000; i ++){
+    if (i % 2500 === 0){
+    alert("quarter of the way there");
+    }if (i % 5000===0){
+     alert("halfway there.");
+    }if (i % 10000===0){
+        alert("loop is done");
+    }}
+
+var myTvshows =["regular show", "adventuretime","the office","walking dead","chowder"]
+console.log ("#1 favorite tv show is",myTvshows[0], )
+console.log ("#2 favorite tv show is",myTvshows[1],)
+console.log ("#3 favorite tv show.",myTvshows[2],)
+console.log ("#4 favorite tv show.",myTvshows[3],)
+console.log ("#5 favorite tv show.",myTvshows[4])
+
